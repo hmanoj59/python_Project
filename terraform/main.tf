170a689000ec1387e5cf52f452339a01b0814246
hmanoj59 <hotasmail@gmail.com>
2019-12-22 01:28:21 -0500

    terraform scripts for kubernetes cluster

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "google" {
+  project = "silken-math-197521"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "naga-aks-cluster"
+  location = "us-east1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = "naga"
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
